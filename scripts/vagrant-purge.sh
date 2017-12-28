1463a5bc7f74a36ba21c1d89d3840f939d155dc7
ambroisemaupate <ambroisemaupate@rezo-zero.com>
2017-12-28 10:02:39 +0100

    Removed DD from purge

--- scripts/vagrant-purge.sh
+++ scripts/vagrant-purge.sh
@@ -59,7 +59,3 @@ sudo find /var/cache -type f -exec rm -rf {} \;
 # delete any logs that have built up during the install
 sudo find /var/log/ -name *.log -exec rm -f {} \;
 
-# Zero free space to aid VM compression
-sudo dd if=/dev/zero of=/EMPTY bs=1M
-sudo rm -f /EMPTY
-
